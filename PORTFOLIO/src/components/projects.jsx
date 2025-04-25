f9599d226d75baecbfdcc8d7ad2892153dbf0964
Arif039 <saif039@gmail.com>
2025-04-25 23:55:11 +0600

    update project
    update project

--- PORTFOLIO/src/components/projects.jsx
+++ PORTFOLIO/src/components/projects.jsx
@@ -25,7 +25,7 @@ export default function Projects () {
                     <img src={supplyImage} alt="supplychain" className='h-90 bg-fixed'/>
                     <h2 className='text-[#697484] px-4'>Decentralize SupplyChain</h2>
                     <h3 className='text-sm  text-[#697484] px-4'>The core benefits of decentralization in a supply chain stem from distributing control and data across multiple participants rather than relying on a central authority.</h3>
-                    <button type='submit'  className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:w-3/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/simple_supply_chain/', '_blank')} >Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
+                    <button type='submit'  className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:w-3/7 xl:w-2/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/simple_supply_chain/', '_blank')} >Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
                 </div>
 
                
@@ -33,35 +33,35 @@ export default function Projects () {
                     <img src={votingImage} alt="voting" className='h-90 bg-fixed'/>
                     <h2 className='text-[#697484] px-4'>Decentralize Voting</h2>
                     <h3 className='text-sm  text-[#697484] px-4'>The core benefits of decentralization in a voting system is the from distributing control and data across multiple participants rather than relying on a central authority.</h3>
-                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:w-3/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300'  onClick={() => window.open('https://arif039.github.io/Voting_dApp/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
+                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:3/7 xl:w-2/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300'  onClick={() => window.open('https://arif039.github.io/Voting_dApp/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
                 </div>
 
                 <div className='flex flex-col gap-4 border-1 border-gray-300  rounded-md shadow-md'>
                     <img src={todoImage} alt="todo" className='h-90 bg-fixed'/>
                     <h2 className='text-[#697484] px-4'>Decentralize ToDo</h2>
                     <h3 className='text-sm  text-[#697484] px-4'>A decentralized to-do app aims to remove reliance on a central server for storing and managing your tasks. Instead, data is typically distributed across a network of computers. </h3>
-                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:w-3/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/todo-app/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
+                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:3/7 xl:w-2/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/todo-app/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
                 </div>
 
                 <div className='flex flex-col gap-4 border-1 border-gray-300  rounded-md shadow-md'>
                     <img src={nftImage} alt="supplychain" className='h-90 bg-fixed'/>
                     <h2 className='text-[#697484] px-4'>ERC20 TOKENS</h2>
                     <h3 className='text-sm  text-[#697484] px-4'>The core benefits of decentralization in a supply chain stem from distributing control and data across multiple participants rather than relying on a central authority.</h3>
-                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:w-3/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/erc20_Token/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
+                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:3/7 xl:w-2/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/erc20_Token/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
                 </div>
 
                 <div className='flex flex-col gap-4 border-1 border-gray-300  rounded-md shadow-md'>
                     <img src={portfolioImage} alt="supplychain" className='h-90 bg-fixed'/>
                     <h2 className='text-[#697484] px-4'>Decentralize Portfolio</h2>
                     <h3 className='text-sm  text-[#697484] px-4'>A decentralized portfolio takes the principles of decentralization – control, transparency, and resilience – and applies them to how you showcase your work, skills, and achievements.</h3>
-                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:w-3/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/decentralize_portfolio1/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
+                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:3/7 xl:w-2/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/decentralize_portfolio1/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
                 </div>
 
                 <div className='flex flex-col gap-4 border-1 border-gray-300  rounded-md shadow-md'>
                     <img src={multiplicationImage} alt="supplychain" className='h-90 bg-fixed'/>
                     <h2 className='text-[#697484] px-4'>Multiplication Table Generator</h2>
                     <h3 className='text-sm  text-[#697484] px-4'>A multiplication table generator is a straightforward tool designed to display the results of multiplying a specific number by a range of consecutive integers. </h3>
-                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:w-3/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/Multiplication-Table-Generator/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
+                    <button className='relative overflow-hidden group flex flex-row gap-4 border-1 rounded-lg ml-4 mb-4 w-3/7 md:3/7 xl:w-2/7 text-sm  cursor-pointer text-violet-600 font-bold px-2 py-3 border-violet-600 hover:scale-105 transition-transform duration-300' onClick={() => window.open('https://arif039.github.io/Multiplication-Table-Generator/', '_blank')}>Case Study <img src={arrowImage} alt="arrowImage" className='transition-transform duration-300 ease-in-out group-hover:translate-x-3'/> </button>
                 </div>
                
 
